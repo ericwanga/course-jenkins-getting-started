2257f34fd9c29c0307311bea1fdf20c462c6396c
Eric Wang <eric.wang@capgemini.com>
2021-08-17 10:14:43 +0930

    update junit and groovy pipeline

--- ide/pipeline.groovy
+++ ide/pipeline.groovy
@@ -15,7 +15,7 @@ pipeline {
         
             post {
                 always {
-                    junit '**/target/surefire-reports/TEST-*.xml'
+                    junit '**/target/surefire-reports/TEST*.xml'
                     archiveArtifacts 'target/*.jar'
                 }
                 changed {
